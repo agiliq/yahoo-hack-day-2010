a1460d06046c45789e7f2251a2fcc03cfc8b930b
Javed Khan <javed@agiliq.com>
2010-07-24 15:37:22 +0530

    fix typo

--- sellflickr/flickrimporter/models.py
+++ sellflickr/flickrimporter/models.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class FlickrUser(models.Model):
     user = models.ForeignKey(User, related_name='flickr_profiles')
 
-class FlickrPhoto(modes.Model):
+class FlickrPhoto(models.Model):
     owner = models.ForeignKey(FlickrUser)
     
     def get_image_url(self, size):
