72668ebd1472423e0e0278e02fdecf3ba9ade8a9
Lakshman Prasad <lakshman@uswaretech.com>
2010-07-24 17:55:22 +0530

    Added admin related changes

--- sellflickr/flickrimporter/admin.py
+++ sellflickr/flickrimporter/admin.py
@@ -2,5 +2,11 @@ from subdomain_admin.admin import SubdomainAdmin
 from django.contrib import admin
 from flickrimporter.models import FlickrPhoto, FlickrUser
 
+class FlickrPhotoAdmin(SubdomainAdmin):
+    list_filter = ('customer','datetime')
+    list_display = ('customer','datetime','get_thumbnail')
+    search_fields = ['title',]
+
+
 admin.site.register(FlickrPhoto)
 admin.site.register(FlickrUser)
