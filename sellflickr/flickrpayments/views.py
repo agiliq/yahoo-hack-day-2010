d781448af3affb437c3c9a68b34dcec576937d78
ashok.raavi <ashok.raavi@gmail.com>
2010-07-24 22:02:45 +0530

    change in photo_detail

--- sellflickr/flickrpayments/views.py
+++ sellflickr/flickrpayments/views.py
@@ -1,10 +1,12 @@
 
 from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.forms.models import modelformset_factory
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.generic.list_detail import object_detail, object_list
+from django.views.decorators.csrf import csrf_exempt
 
 from paypal.standard.forms import PayPalPaymentsForm
 
@@ -35,27 +37,31 @@ def photo_detail(request, object_id):
     paypal_dict = {
         "business": user_paypal.email,
         "amount": photo.get_price,
-        "item_name": photo.title,
+        "item_name": 'payment for photo %s with invoice %s' % (photo.title, payment_obj.pk),
         "invoice": payment_obj.pk,
         "notify_url": notify_url,
         "return_url": return_url,
         "cancel_return": cancel_return,
     }
+    print paypal_dict
+    is_debug = settings.DEBUG
     form = PayPalPaymentsForm(initial=paypal_dict)
     return object_detail(request, 
                          photos, 
                          object_id=object_id, 
                          template_name='flickrpayments/photo_detail.html', 
                          extra_context={'photo_list': photos.order_by('?')[:6],
-                                        'form': form})
+                                        'form': form,
+                                        'is_debug': is_debug})
 
-
+@csrf_exempt
 def buy_done(request, object_id):
     photo = get_object_or_404(FlickrPhoto, pk=object_id, owner__user=get_subdomain_user(request))
     photos = FlickrPhoto.objects.filter(owner__user=get_subdomain_user(request))
     return render_to_response('flickrpayments/buy_done.html', 
                               {'photo': photo, 
-                               'photo_list': photos})
+                               'photo_list': photos},
+                              context_instance=RequestContext(request))
 
 
 def buy_photo(request, object_id):
