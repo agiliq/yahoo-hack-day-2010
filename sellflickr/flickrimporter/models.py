f6c3fc66669cf1110a29b2b39951c623f3e3bcac
Javed Khan <javed@agiliq.com>
2010-07-24 15:35:09 +0530

    models stubbed

--- sellflickr/flickrimporter/models.py
+++ sellflickr/flickrimporter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class FlickrUser(models.Model):
+    user = models.ForeignKey(User, related_name='flickr_profiles')
+
+class FlickrPhoto(modes.Model):
+    owner = models.ForeignKey(FlickrUser)
+    
+    def get_image_url(self, size):
+        return ""
