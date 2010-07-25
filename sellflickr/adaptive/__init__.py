4962981716f70b223fec0792e6b4a07a43177bd1
Shabda Raaj <shabda@uswaretech.com>
2010-07-25 23:06:57 +1000

    Adaptive

---
+++ sellflickr/adaptive/__init__.py
@@ -0,0 +1,24 @@
+def get_adaptive_payment_url(returnUrl, cancelUrl, receiverList, **kwargs):
+    from django.conf import settings
+    API_END_POINT = 'https://svcs.sandbox.paypal.com/AdaptivePayments/Pay'
+    headers = {
+    "X-PAYPAL-SECURITY-USERID": settings.PAYPAL_API_USER,
+    "X-PAYPAL-SECURITY-PASSWORD": settings.PAYPAL_API_KEY,
+    "X-PAYPAL-SECURITY-SIGNATURE": settings.PAYPAL_API_SIG,
+    "X-PAYPAL-DEVICE-IPADDRESS": "117.192.38.155",
+    "X-PAYPAL-REQUEST-DATA-FORMAT": "JSON",
+    "X-PAYPAL-RESPONSE-DATA-FORMAT": "JSON",
+    "X-PAYPAL-APPLICATION-ID":"APP-80W284485P519543T"
+    }
+    data = {"returnUrl":returnUrl, \
+            "requestEnvelope":{"errorLanguage":"en_US"},"currencyCode":"USD", \
+            "receiverList":{"receiver":receiverList},
+            "cancelUrl":cancelUrl,\
+            "actionType":"PAY"
+    }
+    import urllib, urllib2, json
+    request = urllib2.Request(API_END_POINT, data = json.dumps(data), headers = headers)
+    resp = json.loads(urllib2.urlopen(request).read())
+    key = resp["payKey"]
+    return "https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&paykey=%s"%key
+    
