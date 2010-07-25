8e38eb202ac7a6a6a2fd02443cd247a554af3c7d
ashok.raavi <ashok.raavi@gmail.com>
2010-07-25 10:22:28 +0530

    style changes

--- sellflickr/flickrpayments/urls.py
+++ sellflickr/flickrpayments/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('flickrpayments.views',
     url(r'^set-price/$', 'set_price', name='flickrpayments_set_price'),
     url('^detail/(?P<object_id>\d+)/$', 'photo_detail', name='flickrpayments_photo_detail'),
-    url('^buy/(?P<object_id>\d+)/$', 'buy_photo', name='flickrpayments_photo_buy'),
+    # url('^buy/(?P<object_id>\d+)/$', 'buy_photo', name='flickrpayments_photo_buy'),
     url('^buy/done/(?P<object_id>\d+)/$', 'buy_done', name='flickrpayments_buy_done'),
     url('^myconfig/$', 'my_config', name='admin_configure'),
     url('^mysite/$', 'my_site', name='my_site'),
---
+++ sellflickr/subdomains/admin.py
@@ -0,0 +1,6 @@
+
+from django.contrib import admin
+
+from subdomains.models import Subdomain
+
+admin.site.register(Subdomain)
